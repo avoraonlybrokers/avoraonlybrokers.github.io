5408dc65394e5949c13f51f0038cdd977de26473
avoraonlybrokers <rthdtest44@gmail.com>
2026-07-19 21:23:14 +0300

    Delete sql directory

--- sql/seed_developers.sql
+++
@@ -1,8 +0,0 @@
-insert into developers (name_ru, name_en, website_url, sort_order) values
-  ('Sansara Development Group', 'Sansara Development Group', 'https://sansaradevelopment.com/en', 1),
-  ('Major Developments',        'Major Developments',        'https://majordevelopments.ae/',      2),
-  ('Layan Group',                'Layan Group',                'https://layanverde.com/ru',           3),
-  ('Imtiaz',                     'Imtiaz',                     'https://imtiaz.ae/',                  4),
-  ('Binghatti',                  'Binghatti',                  'https://www.binghatti.com/',          5),
-  ('Beyond Developments',        'Beyond Developments',        'https://beyonddevelopments.ae/',      6)
-on conflict do nothing;
